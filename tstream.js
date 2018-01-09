6cbda68eb77f1cc55968772c5bfb7c5a0665a38d
BDFOZ <Ben.Faus@live.com>
2018-01-08 21:25:42 -0600

    added animate() to javascript

--- tstream.js
+++ tstream.js
@@ -1,1 +1,7 @@
 //can use .animate(hide) to move in/out the menu elements
+console.log("linked");
+$(".off-on").on("mouseover",function(){
+   $(".off-on").animate({width:"100px"},500);
+   $(".off-on").animate({width:"10px"},500);
+});
+// $(".off-on").off("mouseover");
